e3dac829937bbbb6fcc80dd5a21a9ce795eea513
SirTarragon <40471000+SirTarragon@users.noreply.github.com>
2020-07-20 23:40:17 -0400

    v.0.0.1
    First level designed, more assets. Smoke particle made for future collision particle system.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     private Rigidbody rigidBody;
-    private AudioSource audioSource;
     private double fuelValue;
+
+    [SerializeField] float rcsThrust = 100;
+    [SerializeField] float mainThrust = 25;
 
     // Use this for initialization
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
         fuelValue = 100;
     }
 
@@ -27,34 +28,71 @@ public class Rocket : MonoBehaviour {
             print(fuelValue + " units of Fuel remaining.");
 
             //Rocket thrust
-            if (Input.GetKey(KeyCode.Space))
-            {
-                print("Thrusting");
-                rigidBody.AddRelativeForce(Vector3.up);
-                fuelValue -= 0.05;
-            }
-            else if (Input.GetKeyUp(KeyCode.Space))
-            {
-                print("No Thrust");
-            }
+            Thrust();
 
             //Rocket turning
-            if (Input.GetKey(KeyCode.A))
-            {
-                print("Rotating Left");
-                transform.Rotate(Vector3.forward);
-                fuelValue -= 0.01;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                print("Rotating Right");
-                transform.Rotate(-Vector3.forward);
-                fuelValue -= 0.01;
-            }
+            Rotation();
         }
         else
         {
             print("No fuel remaining");
+        }
+    }
+
+    private void Rotation()
+    {
+        rigidBody.freezeRotation = true; //Take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+            fuelValue -= 0.01;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+            fuelValue -= 0.01;
+        }
+
+        rigidBody.freezeRotation = false; //Resume physics control
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            fuelValue -= 0.05;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            print("No Thrust");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                print("Friendly Collision");
+                break;
+            case "Fuel":
+                fuelValue += 25;
+                print("Refueled, fuel is now: " + fuelValue + " Units of Fuel.");
+                break;
+            default:
+                print("Player is dead.");
+                // kill player
+                fuelValue = 0;
+                // reload level
+                break;
+
         }
     }
 
